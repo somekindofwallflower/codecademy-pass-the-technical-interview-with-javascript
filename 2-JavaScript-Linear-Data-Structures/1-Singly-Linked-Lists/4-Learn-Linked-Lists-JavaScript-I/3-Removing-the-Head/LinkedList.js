5b2389493c3d6b62528088758044e675d0338ff5
somekindofwallflower <someone.ana13@gmail.com>
2023-01-06 23:26:22 +0100

    Did 2-JavaScript-Linear-Data-Structures / 1-Singly-Linked-Lists / 4-Learn-Linked-Lists-JavaScript-I / 3-Removing-the-Head

---
+++ 2-JavaScript-Linear-Data-Structures/1-Singly-Linked-Lists/4-Learn-Linked-Lists-JavaScript-I/3-Removing-the-Head/LinkedList.js
@@ -0,0 +1,39 @@
+const Node = require('./Node');
+
+class LinkedList {
+  
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if(!removedHead) return;
+    this.head = removedHead.getNextNode();
+    return removedHead.data;
+  }
+
+}
+
+module.exports = LinkedList;

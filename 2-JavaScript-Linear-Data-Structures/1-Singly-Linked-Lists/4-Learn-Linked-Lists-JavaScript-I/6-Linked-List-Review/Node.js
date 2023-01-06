aa5a4345f015129a224482bcc0da1c4de733ed6d
somekindofwallflower <someone.ana13@gmail.com>
2023-01-06 23:27:38 +0100

    Did 2-JavaScript-Linear-Data-Structures / 1-Singly-Linked-Lists / 4-Learn-Linked-Lists-JavaScript-I / 6-Linked-List-Review

---
+++ 2-JavaScript-Linear-Data-Structures/1-Singly-Linked-Lists/4-Learn-Linked-Lists-JavaScript-I/6-Linked-List-Review/Node.js
@@ -0,0 +1,20 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  
+    setNextNode(node) {
+      if (node instanceof Node || node === null) {
+        this.next = node;
+      } else {
+        throw new Error('Next node must be a member of the Node class.');
+      }
+    }
+  
+    getNextNode() {
+      return this.next;
+    }
+  }
+  
+  module.exports = Node;
